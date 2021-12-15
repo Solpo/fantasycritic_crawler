374818ca7a97c2d3949d6c17a102a0813f5737f9
Solpo <3440817+Solpo@users.noreply.github.com>
2021-12-15 17:13:42 +0200

    nimet haettiin väärästä paikasta, korjattu

--- fantasy_critic_crawler.py
+++ fantasy_critic_crawler.py
@@ -9,7 +9,7 @@ class Julkaisija:
 
     async def init(self, page: "Page", peleja: int):
         # print(f"Haetaan pelaajan nimi, peleja on {peleja}")
-        self.nimi = str(await paikka_tekstiksi(f"#__BVID__36 > tbody:nth-child(2) > tr:nth-child({self.numero_str}) > td:nth-child(2) > span:nth-child(1) > a:nth-child(1)", page)).strip()
+        self.nimi = str(await paikka_tekstiksi(f"div.col-xl-6:nth-child({self.numero_str}) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1)", page)).strip()
         self.kokonaispisteet = int(await paikka_tekstiksi(f"div.col-xl-6:nth-child({self.numero_str}) > div:nth-child(2) > table:nth-child(2) > tbody:nth-child(2) > tr:nth-child({str(peleja + 2)}) > td:nth-child(2)", page))
         self.pelit = []
         for i in range(1, peleja + 1):
